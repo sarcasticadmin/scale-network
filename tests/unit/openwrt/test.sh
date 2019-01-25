b9d82a3863bf584ce4559ecb60aada9726097784
Robert James Hernandez <rob@sarcasticadmin.com>
2019-01-24 17:47:38 -0800

    WIP

--- tests/unit/openwrt/test.sh
+++ tests/unit/openwrt/test.sh
@@ -3,12 +3,9 @@ OUTPUT="tmp"
 
 gomplate -d openwrt=../../../facts/secrets/openwrt.yaml --input-dir=../../../openwrt/files --output-dir="./${OUTPUT}"
 
-find -s ./${OUTPUT} -type f -exec md5 {} \; > ./${OUTPUT}/SUM
+cd ./${OUTPUT}
+#find -s . -type f -exec md5 {} \; > ./SUM
+find . -type f -exec md5 {} \; > ./SUM
+cd ..
 
-while read cat file
-do
-    matches=$( grep ./${OUTPUT}/SUM )
-    echo "hi"
-    [[ $matches != '' ]] && echo "$file:"$'\n'"$matches"
-done < ./golden/SUM
-#diff ./${OUTPUT}/SUM ./golden/SUM
+diff -u ./${OUTPUT}/SUM ./golden/SUM
