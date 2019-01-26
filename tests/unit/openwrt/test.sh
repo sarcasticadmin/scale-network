4029181c58a79102432efd28583aa828220583be
Robert James Hernandez <rob@sarcasticadmin.com>
2019-01-25 20:40:06 -0800

    Still working it out

--- tests/unit/openwrt/test.sh
+++ tests/unit/openwrt/test.sh
@@ -1,11 +1,61 @@
+usage(){
+  cat << EOF
+usage: $(basename $0) [OPTIONS] ARGS
+
+Simple template of getopts
+
+OPTIONS:
+  -h      Show this message
+  -e      Echo first arg
+
+EXAMPLES:
+  To print out arg1:
+
+      $(basename $0) arg1
+
+EOF
+}
+
+while getopts "hu" OPTION
+do
+  case $OPTION in
+    u )
+      UPDATE=1
+      ;;
+    h )
+      usage
+      exit 0
+      ;;
+    \? )
+      usage
+      exit 1
+      ;;
+  esac
+done
+shift $((OPTIND -1))
+
+UPDATE=${UPDATE:-0}
+
+
 #OUTPUT="golden"
 OUTPUT="tmp"
 
-gomplate -d openwrt=../../../facts/secrets/openwrt.yaml --input-dir=../../../openwrt/files --output-dir="./${OUTPUT}"
+gen_templates(){
+  gomplate -d openwrt=../../../facts/secrets/openwrt.yaml --input-dir=../../../openwrt/files --output-dir="./${1}"
+}
 
-cd ./${OUTPUT}
+if [ "${UPDATE}" == "1" ]; then
+  gen_templates golden
+fi
+
+gen_templates tmp
+
+# Exclude network for now since maps are in random order
+# see: https://github.com/hairyhenderson/gomplate/issues/457
+diff -r golden/ tmp/ -x "*network*"
+
+#cd ./${OUTPUT}
 #find -s . -type f -exec md5 {} \; > ./SUM
-find . -type f -exec md5 {} \; > ./SUM
-cd ..
-
-diff -u ./${OUTPUT}/SUM ./golden/SUM
+#find . -type f -exec md5 {} \; > ./SUM
+#cd ..
+#diff -u ./${OUTPUT}/SUM ./golden/SUM
